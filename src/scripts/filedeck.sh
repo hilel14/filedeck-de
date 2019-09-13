9650c2d9e13a1b0f1aa3be5d956ed3144a6dc89d
Hilel New <hilel14@gmail.com>
2019-09-13 11:27:31 +0300

    change launch script

--- src/scripts/filedeck.sh
+++ src/scripts/filedeck.sh
@@ -3,6 +3,12 @@
 # make sure logs folder exist
 mkdir -p ~/logs
 
-# launch filedeck
+# launch filedeck only if user is connected to root-folder
 APP_HOME=/opt/hilel14/filedeck
-java -Djava.util.logging.config.file=$APP_HOME/logging.properties -cp .:$APP_HOME/lib/* org.hilel14.filedeck.de.Main
+if [ -d $APP_HOME ]; then
+	java -Djava.util.logging.config.file=$APP_HOME/logging.properties -cp .:$APP_HOME/lib/* org.hilel14.filedeck.de.Main
+else
+   echo "You are not connected to RootFolder"
+fi
+
+
