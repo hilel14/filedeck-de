c99eb8449b47bc0473d41f7d9c66964517e0beb8
hilel14 <hilel14@gmail.com>
2019-10-18 09:09:36 +0300

    change launch script

--- src/scripts/filedeck.sh
+++ src/scripts/filedeck.sh
@@ -3,12 +3,17 @@
 # make sure logs folder exist
 mkdir -p ~/logs
 
-# launch filedeck only if user is connected to root-folder
-APP_HOME=/opt/hilel14/filedeck
-if [ -d $APP_HOME ]; then
-	java -Djava.util.logging.config.file=$APP_HOME/logging.properties -cp .:$APP_HOME/lib/* org.hilel14.filedeck.de.Main
-else
-   echo "You are not connected to RootFolder"
-fi
+# set application folder relative to the location of this scripts (remark next 2 lines to set application folder path directly)
+APP_HOME=`dirname $0`
+APP_HOME=`dirname $APP_HOME`
+
+# or set application folder directly
+APP_HOME=/Volumes/RootFolder/FileDeck
+
+# launch filedeck
+java -Djava.util.logging.config.file=$APP_HOME/resources/logging.properties -cp $APP_HOME/resources:$APP_HOME/lib/* org.hilel14.filedeck.de.Main
 
 
+# copy and paste into Apple Script Editor
+# do shell script "/path/to/this/script"
+# then export as application
